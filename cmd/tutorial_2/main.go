c146be80981b9a42cbfa19f5fdc8945c1a5d779b
yaredow <yaredyilma11@gmail.com>
2026-03-02 12:21:53 +0300

    feat(pointers): finish pointers and write some codes

--- cmd/tutorial_2/main.go
+++ cmd/tutorial_2/main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var p *int32
-	fmt.Println(p)
+	thing := [5]float64{1, 2, 3, 4, 5}
+	squared := square(thing)
+	fmt.Println(squared)
+}
+
+func square(thing [5]float64) [5]float64 {
+	for i := 0; i < len(thing); i++ {
+		thing[i] = thing[i] * thing[i]
+	}
+
+	return thing
 }
